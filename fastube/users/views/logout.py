5fac3bf94a295b353f11d26bcd5e8a14dedd29ab
jupiny <tmdghks584@gmail.com>
2016-06-21 15:53:43 +0900

    update LogoutView: add success logout flash message

--- fastube/users/views/logout.py
+++ fastube/users/views/logout.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.contrib.auth import logout
@@ -8,4 +9,9 @@ class LogoutView(View):
 
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            "성공적으로 로그아웃 되었습니다.",
+        )
         return redirect(reverse("login"))
