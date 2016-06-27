04612b02c3da742f0a5e96406e5ce2b1fe27a7ee
jupiny <tmdghks584@gmail.com>
2016-06-27 17:34:27 +0900

    update posts.js: render comments count and list via ajax request

--- fastube/fastube/urls/api.py
+++ fastube/fastube/urls/api.py
@@ -5,5 +5,5 @@ from posts.api import *
 
 urlpatterns = [
     url(r'^posts/$', PostListAPIView.as_view(), name="posts"),
-    url(r'^(?P<slug>\w+)/comments/$', PostCommentsListAPIView.as_view(), name="comments"),
+    url(r'^posts/(?P<slug>\w+)/comments/$', PostCommentsListAPIView.as_view(), name="comments"),
 ]
