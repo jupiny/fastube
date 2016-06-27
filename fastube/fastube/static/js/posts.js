81686c16411b57885a97cdc79145428b5183a0de
jupiny <tmdghks584@gmail.com>
2016-06-27 16:51:38 +0900

    update posts.js: alert comments content on conmments create from submit

---
+++ fastube/fastube/static/js/posts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var commentsSectionElement = $("#comments-section");
+    var commentsCreateFormElement = $(commentsSectionElement).find("form");
+    var commentsCreateFormInputContentElement = $(commentsCreateFormElement).find("input[name='content']");
+
+    commentsCreateFormElement.submit(function() {
+        var content = $(commentsCreateFormInputContentElement).val();
+        alert(content);
+        $(commentsCreateFormInputContentElement).val("")
+    return false;
+    });
+});
